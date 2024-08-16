1d820915dcca2a90b7cd7cc0b9e9b144164d2a4e
TI Sabbir <tisabbir0@gmail.com>
2024-08-16 16:10:28 +0600

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Root</div>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <div>Child ROutes</div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
