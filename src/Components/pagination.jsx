aacc14cde59820701e4e3d0f02434918e15173d6
TI Sabbir <tisabbir0@gmail.com>
2024-08-16 23:34:56 +0600

    pagination added

---
+++ src/Components/pagination.jsx
@@ -0,0 +1,13 @@
+const pagination = () => {
+  return (
+    <div>
+      <div className="join">
+        <button className="join-item btn">«</button>
+        <button className="join-item btn">Page 22</button>
+        <button className="join-item btn">»</button>
+      </div>
+    </div>
+  );
+};
+
+export default pagination;
