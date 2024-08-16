82b89b0f1fe6bb5fc8a9f3982c6ade6ca25fd4bb
TI Sabbir <tisabbir0@gmail.com>
2024-08-16 23:29:35 +0600

    product load done

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaStar, FaStarHalf } from "react-icons/fa";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -12,20 +13,35 @@ const Products = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {products.map((product, index) => (
-        <div key={index}>
+        <div key={index} className="mx-auto">
           <div className="card bg-base-100 w-96 shadow-xl">
-            <figure className="px-10 pt-10">
+            <figure className="">
               <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                src={product.image}
                 alt="Shoes"
                 className="rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
+              <h2 className="card-title">{product.name}</h2>
+              <p className="font-semibold">{product.brand}</p>
+              <p>{product.description}</p>
+              <div className="">
+               <div className="flex items-center gap-2">
+               <div className="flex">
+                    <FaStar className="text-yellow-600" />
+                    <FaStar className="text-yellow-600" />
+                    <FaStar className="text-yellow-600" />
+                    <FaStar className="text-yellow-600" />
+                    <FaStarHalf className="text-yellow-600" />
+                </div>
+                <div>
+                    <p>{product.ratings}</p>
+                </div>
+               </div>
+              <div>
+                <p className="text-xl font-bold">TK {product.price}</p>
+              </div>
               </div>
             </div>
           </div>
