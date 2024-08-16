8d114e95ff23d87d6fd4ce412b21e34c037a774c
TI Sabbir <tisabbir0@gmail.com>
2024-08-16 23:37:25 +0600

    search bar

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <div className="join">
+        <input className="input input-bordered join-item" placeholder="Item Name" />
+        <button className="btn join-item rounded-r-full">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/Components/pagination.jsx
+++ src/Components/pagination.jsx
@@ -2,9 +2,9 @@ const pagination = () => {
   return (
     <div>
       <div className="join">
-        <button className="join-item btn">«</button>
+        <button className="join-item btn">« previous</button>
         <button className="join-item btn">Page 22</button>
-        <button className="join-item btn">»</button>
+        <button className="join-item btn">next »</button>
       </div>
     </div>
   );
